e1240d71cac6c32e50f01299be7a5c4fe6453a63
vcgracie <vzc0048@auburn.edu>
2024-11-08 12:58:16 -0600

    Create solflspectrum.py

---
+++ solflspectrum.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class SolFlSpectrum():
+    def __init__(self,fullpath):
+        self.fullpath = fullpath
+
+        
+        data = pd.read_csv(fullpath,skiprows=12,sep='\t',names =['wl','counts','extra'])
+        data = data.drop(columns='extra')
+        data = data.dropna()
+
+
+        self.data = data
+        self.bg = np.mean(self.data['counts'][0:10])
+        self.data['flcorr']=self.data['counts']-self.bg
+        self.data['flnorm']=self.data['flcorr']/np.max(self.data['flcorr'])
+        
+        plt.figure()
+        plt.plot(self.data['wl'],self.data['flcorr'])
+        plt.xlabel('Wavelength [nm]')
+        plt.ylabel('Fluorescence [arb.u.]')
+        
+        print(data)
+        
+        
+    def saveSpectrum(self,outputpath):
+        self.data.to_csv(outputpath + "PROCESSED_" + self.name,index = False)
