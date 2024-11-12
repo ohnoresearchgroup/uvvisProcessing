13c382b84bf17b097c78a54f3d5c64668ab64719
Paul Ohno <pohno@users.noreply.github.com>
2024-11-12 13:18:52 -0600

    updated absspectrum
    updated to not do automatic baseline correction. now allows baseline correction as optional argument where a wavelength range is included

--- absspectrum.py
+++ absspectrum.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 class AbsSpectrum():
-    def __init__(self,fullpath,horiba=False):
+    def __init__(self,fullpath,horiba=False,bgrange=None):
         self.fullpath = fullpath
 
         if horiba is False:
@@ -18,8 +18,18 @@ class AbsSpectrum():
 
 
         self.data = data
-        self.bg = np.mean(self.data['abs'][0:10])
-        self.data['abscorr']=self.data['abs']-self.bg
+        if bgrange is None:
+            #set abscorr equal to abs
+            self.data['abscorr'] = self.data['abs']
+        else:
+            #find indices of bgrange
+            lidx = closest_index(self.data['wl'],bgrange[0])
+            ridx = closest_index(self.data['wl'],bgrange[1])
+            
+            self.bg = np.mean(self.data['abs'][lidx:ridx])
+            self.data['abscorr']=self.data['abs']-self.bg
+        
+        #normalized data
         self.data['absnorm']=self.data['abscorr']/np.max(self.data['abscorr'])
         
         plt.figure()
@@ -32,3 +42,8 @@ class AbsSpectrum():
         
     def saveSpectrum(self,outputpath):
         self.data.to_csv(outputpath + "PROCESSED_" + self.name,index = False)
+        
+        
+def closest_index(array, target):
+    # Find the index of the element with the smallest difference to the target
+    return min(range(len(array)), key=lambda i: abs(array[i] - target))
